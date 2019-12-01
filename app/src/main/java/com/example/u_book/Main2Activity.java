cedb696e040359709e94489df907891eb0fafb10
Luis14718 <luis14718@gmail.com>
2019-12-01 11:33:43 -0600

    navigation panel

--- app/src/main/java/com/example/u_book/Main2Activity.java
+++ app/src/main/java/com/example/u_book/Main2Activity.java
@@ -4,9 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
@@ -20,12 +24,21 @@ public class Main2Activity extends AppCompatActivity implements NavigationView.O
         setContentView(R.layout.activity_main2);
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
+        init();
 
+    }
+
+    private void init() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout);
+        NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         switch(menuItem.getItemId()){
+
 
             case R.id.nav_profile:{
 
@@ -36,14 +49,34 @@ public class Main2Activity extends AppCompatActivity implements NavigationView.O
 
             case R.id.nav_posts:{
 
-             
+                Navigation.findNavController(this, R.id.nav_host_fragment).navigate(R.id.postsScreen);
 
                 break;
             }
+
         }
         menuItem.setChecked(true);
         drawerLayout.closeDrawer(GravityCompat.START);
 
         return true;
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.logout:{
+
+                return true;
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
