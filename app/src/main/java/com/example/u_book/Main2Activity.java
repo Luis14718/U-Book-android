31facbf4c94686b811c7d1b5601e27fc9cbfae00
Luis14718 <luis14718@gmail.com>
2019-12-01 11:01:09 -0600

    drawer layout
    navigation

--- app/src/main/java/com/example/u_book/Main2Activity.java
+++ app/src/main/java/com/example/u_book/Main2Activity.java
@@ -1,14 +1,49 @@
 package com.example.u_book;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class Main2Activity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+public class Main2Activity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch(menuItem.getItemId()){
+
+            case R.id.nav_profile:{
+
+                Navigation.findNavController(this, R.id.nav_host_fragment).navigate(R.id.profileScreen);
+
+                break;
+            }
+
+            case R.id.nav_posts:{
+
+             
+
+                break;
+            }
+        }
+        menuItem.setChecked(true);
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 }
